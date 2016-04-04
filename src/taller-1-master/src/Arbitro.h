e6f627c9c97064998d3b24ec420f49f782940956
geraldinevillaneda <geraldine.villaneda@correounivalle.edu.co>
2016-04-04 16:30:12 -0500

    Added files via upload

---
+++ src/taller-1-master/src/Arbitro.h
@@ -0,0 +1,26 @@
+//Clase: Abitro
+//Responsablidad: Es el encargado de realizar y controlar el juego 
+//Colaboración:  la clase arbitro utiliza la clase tablero.
+#ifndef ARBITRO_H_
+#define ARBITRO_H_
+#include "Tablero.h"
+class Arbitro {
+	private:
+	    Tablero tablero;
+	bool juegoEnCurso;
+	double puntaje;
+	int filaBolita;
+	int columnaBolita;
+	public:
+	    Arbitro();
+	~Arbitro();
+	void iniciarJuego();
+	void imprimirTablero();
+	bool validarIntento(int fila, int columna);
+	int calcularDistancia(int fila, int columna);
+	int validaEntrada(int inf, int sup);
+	// metodo implementado para validar las entradas de numeros enteros.
+}
+;
+#endif 
+/* ARBITRO_H_ */
---
+++ src/taller-1-master/src/Tablero.cpp
@@ -0,0 +1,45 @@
+//TALLER 1.0
+// Name        : Tablero.cpp
+// Author      : Geraldine Lopez Villaneda- Maria Alejandra Bernal Toro
+// Version     : 0.1
+	// Copyright   : Geraldine Lopez - Alejandra Bernal 
+	// Description : juego ¿Donde esta la bolita? en c++
+	//============================================================================
+	#include <iostream>
+	#include "Tablero.h"
+	using namespace std;
+	Tablero::Tablero() {
+		dimension = 0;
+				casillas = NULL;
+			}
+			Tablero::Tablero(int n) {
+				//Debe realizar la inicializacion de las variables miembro de la clase Tablero considerando una entrada n
+	dimension = n;
+	casillas = new char* [n];
+	
+	for (int i = 0; i < n; i++) {
+		casillas [i] = new char [n];
+		
+		for (int j = 0; j< n; j++) {
+			casillas[i][j]= 'X';
+			}
+		}
+	}
+	
+	Tablero::~Tablero() {
+		// TODO Auto-generated destructor stub
+	}
+	
+	char Tablero::getCasilla(int x, int y) {
+		return casillas[x][y];
+	}
+	
+	void Tablero::setCasilla(int x, int y, char value) {
+	casillas[x][y] = value;
+	//Debe asignarle un valor de entrada "value" a una casilla del tablero (x,y)
+}
+
+int Tablero::getDimension() {
+	return dimension;
+	//Debe devolver la dimension del Tablero
+}
